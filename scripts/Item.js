ed2efd0e0db8ed0e13d9bc243684ec663259b5b6
Scott Ian <sim2487@gmail.com>
2018-11-05 09:19:28 -0800

    added item factor and validator

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,16 +1,31 @@
 // eslint-disable-next-line no-unused-vars
 /* global shoppingList, cuid */
 
-// Objective: Build an Item module that 
+// Objective: Build an Item module that
 //contains functions to validate and create items.
 
 'use strict';
 
 const Item = (function () {
 
-const validateName = 
+  function validateName(name) {
+    if (name === ''){
+      throw new TypeError('Name does not exist');
+    }
+  }
 
-return {};
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false
+    };
+
+  };
+
+  return {
+    validateName,
+    create
+  };
 
 }());
-
