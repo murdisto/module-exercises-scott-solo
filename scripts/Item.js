b0d208f351f8eac6cc245dc7ef31f5cd2e34e25d
Jordan <jordanhaddadi@gmail.com>
2018-11-05 11:47:19 -0500

    creating our Item module

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 /* global shoppingList, cuid */
+
+// Objective: Build an Item module that 
+//contains functions to validate and create items.
+
 'use strict';
 
 const Item = (function () {
 
-
+const validateName = 
 
 return {};
 
